292b8862379a499f71e2633ef6e9193558829622
shlee9412 <rlsh7321@gmail.com>
2021-05-17 02:13:44 +0900

    2021-05-17 02:13:44

--- src/routers/testRouter.ts
+++ src/routers/testRouter.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const testRouter = Router();
 
 testRouter.get('/', (req, res) => {
-  res.send('<h1>Hello World !!</h1>');
+  res.send('<h1>RestAPI Test</h1>');
 });
 
 export default testRouter;
